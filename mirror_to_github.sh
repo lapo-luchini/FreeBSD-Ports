bde19003c7d84aff0a669d11caee2557700eab06
Lapo Luchini <lapo@lapo.it>
2023-08-21 15:55:03 +0000

    Import improvements from asn1js.

--- mirror_to_github.sh
+++ mirror_to_github.sh
@@ -1,9 +1,11 @@
 #!/bin/sh
 set -e
 cd `dirname "$0"`
+mtn sy
 if [ ! -d .git ]; then
     git init --initial-branch=trunk
     git remote add origin git@github.com:lapo-luchini/FreeBSD-Ports.git
+    rm git-marks1.txt git-marks2.txt
 fi
 touch git-marks1.txt git-marks2.txt
 mtn --quiet --authors=git-authors.txt --branches-file=git-branches.txt --import-marks=git-marks1.txt --export-marks=git-marks1.txt git_export | \
